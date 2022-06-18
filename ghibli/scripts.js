380f59dee301e4bf00043ee2010c127e7f659174
JEJE <99283475+gvalencios@users.noreply.github.com>
2022-06-18 13:31:48 +0800

    reset

--- ghibli/scripts.js
+++ ghibli/scripts.js
@@ -1,3 +1,13 @@
+const app = document.getElementById('root');
+
+const logo = document.createElement('img');
+logo.src = 'logo.png';
+
+const container = document.createElement('div');
+container.setAttribute('class', 'container');
+
+app.appendChild(logo);
+app.appendChild(container);
 
 var request = new XMLHttpRequest();
 request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
